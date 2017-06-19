5610d48d54cff8f7773dcf362f73f965deb86b79
srebalaji <srebalaji@outlook.com>
2017-06-19 15:37:55 +0530

    Adding expense model
    1. Adding expense model.
    2. Adding expenses collections.
    3. Adding basic validations.

---
+++ js/models/expense.js
@@ -0,0 +1,33 @@
+// model
+var app = app || {};
+app.Expense = Backbone.Model.extend({
+	validate: function(attributes){
+		if (!attributes.title){
+			return "Title cant be blank";
+		}
+		if(!attributes.amount){
+			return "Amount cant be blank";
+		}
+		if(attributes.title.search(/^[a-zA-Z]*$/) === -1){
+			return "Title should contain only letters";
+		}
+		if(attributes.amount.search(/^[0-9]+$/) === -1){
+			return "Amount should contain only numbers";
+		}
+	}
+});
+
+app.Expenses = Backbone.Collection.extend({
+	model: app.Expense,
+	url: "http://localhost:3000/v1/expenses"
+});
+
+app.ExpensesInstance = new app.Expenses();
+app.ExpensesInstance.fetch({
+	success: function(response) {
+		console.log("success in fetching expense");
+	},
+	error: function(response) {
+		console.log("failure in fetching expense");
+	}
+});
